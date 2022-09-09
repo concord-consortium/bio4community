d13d570ffb41cad7976f7b2203c3766bf9fc9ae6
Teale Fristoe <fristoe@gmail.com>
2022-09-09 08:12:58 -0700

    Test each page

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -1,11 +1,23 @@
 context("Test the overall app", () => {
-  beforeEach(() => {
-    cy.visit("/?mode=animation&organ=heart");
-  });
-
-  describe("Desktop functionalities", () => {
-    it("renders with text", () => {
-      cy.get(".app .title-box").first().should("have.text", "Plaque Animation");
+  interface pageInfo {
+    mode: string;
+    organ: string;
+    title: string;
+  }
+  const pages: pageInfo[] = [
+    {mode: "animation", organ: "heart", title: "Plaque Animation"},
+    {mode: "animation", organ: "nose", title: "Immune Response Animation"},
+    {mode: "animation", organ: "brain", title: "Focus Animation"},
+    {mode: "simulation", organ: "heart", title: "Plaque Model Simulator"},
+    {mode: "simulation", organ: "nose", title: "Immune Response Model Simulator"},
+    {mode: "simulation", organ: "brain", title: "Focus Model Simulator"},
+  ];
+  describe("Titles are correct", () => {
+    pages.forEach(({ mode, organ, title }: pageInfo) => {
+      it(`renders the title for the ${organ} ${mode}`, () => {
+        cy.visit(`/?mode=${mode}&organ=${organ}`);
+        cy.get(".app .title-box").first().should("have.text", title);
+      });
     });
   });
 });
