157bd1623dcebec06b469cd827bde7718e6385f4
Teale Fristoe <fristoe@gmail.com>
2022-09-13 17:42:06 -0700

    Add some video related cypress tests

--- cypress/integration/workspace.test.ts
+++ cypress/integration/workspace.test.ts
@@ -1,4 +1,17 @@
+
 context("Test the overall app", () => {
+  const visitPage = (mode: string, organ: string) => {
+    cy.visit(`/?mode=${mode}&organ=${organ}`);
+  }
+  const getPlayButton = (first: boolean) => {
+    const gpb = () => cy.get(".app .video-view .video-view-button");
+    if (first) {
+      return gpb().first();
+    } else {
+      return gpb().last();
+    }
+  };
+
   interface pageInfo {
     mode: string;
     organ: string;
@@ -15,9 +28,32 @@ context("Test the overall app", () => {
   describe("Titles are correct", () => {
     pages.forEach(({ mode, organ, title }: pageInfo) => {
       it(`renders the title for the ${organ} ${mode}`, () => {
-        cy.visit(`/?mode=${mode}&organ=${organ}`);
+        visitPage(mode, organ);
         cy.get(".app .title-box").first().should("have.text", title);
       });
+    });
+  });
+
+  describe("Simulation tissue video", () => {
+    beforeEach(() => {
+      visitPage("simulation", "heart");
+    });
+    it(`renders the video title`, () => {
+      cy.get(".app .video-view .video-title").first().should("have.text", "Simulated Artery");
+    });
+    it(`play button works`, () => {
+      getPlayButton(true).should("have.text", "Play");
+      getPlayButton(true).click();
+      getPlayButton(true).should("have.text", "Pause");
+      getPlayButton(true).click();
+      getPlayButton(true).should("have.text", "Play");
+    });
+    it(`can skip by clicking timeline mark label`, () => {
+      const getLastLabel = () => cy.get(".app .video-view").first().find(".rc-slider-mark-text").last();
+      getPlayButton(true).click();
+      getPlayButton(true).should("have.text", "Pause");
+      getLastLabel().click();
+      getPlayButton(true).should("have.text", "Play");
     });
   });
 });
