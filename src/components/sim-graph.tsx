c959a1dc1a69cd1c34c590abc0884b5de090ffba
Teale Fristoe <fristoe@gmail.com>
2022-10-13 13:41:44 -0700

    Address QA feedback for graphs (#21)
    * Give graph a buffer and add right border

--- src/components/sim-graph.tsx
+++ src/components/sim-graph.tsx
@@ -6,22 +6,25 @@ import { AppContext } from "../hooks/use-app-context";
 
 import "./sim-graph.scss";
 
+// Constants
+const graphMargin = 8; // The graph has this margin to the left, right, and bottom, but not top
+const width = 307;
+const height = 95;
+const defaultLineColor = "#949494";
+
 // Returns the min and max values for the given axis of the given data
 const getRange = (data: Coord[], coord: "x" | "y") => ({
   min: Math.min(...data.map(d => d[coord])),
   max: Math.max(...data.map(d => d[coord]))
 });
 
-// Pale gray lines creating a grid as the graph background
-interface IGrid {
+// White rectangle and pale gray lines creating a grid as the graph background
+interface IGraphBackgroundGrid {
   color?: string;
-  height: number;
   hLines: number;
   vLines: number;
-  width: number;
 }
-const Grid = ({ color, height, hLines, vLines, width }: IGrid) => {
-  const defaultColor = "#949494";
+const GraphBackgroundGrid = ({ color, hLines, vLines }: IGraphBackgroundGrid) => {
   const hStep = height / (hLines + 1);
   const hArray = [...Array(hLines).keys()]; // Like range(hLines) in python
   const vStep = width / (vLines + 1);
@@ -29,29 +32,28 @@ const Grid = ({ color, height, hLines, vLines, width }: IGrid) => {
 
   return (
     <>
+      <rect x={graphMargin} y="0" height={height} width={width} />
       {hArray.map(index => {
         const y = hStep * (index + 1);
-        return <line x1="0" y1={y} x2={width} y2={y} stroke={color || defaultColor}
+        return <line x1={graphMargin} y1={y} x2={width + graphMargin} y2={y} stroke={color || defaultLineColor}
           strokeWidth="1" key={`hLine${index}`} />;
       })}
       {vArray.map(index => {
         const x = vStep * (index + 1);
-        return <line x1={x} y1="0" x2={x} y2={height} stroke={color || defaultColor}
+        return <line x1={x + graphMargin} y1="0" x2={x + graphMargin} y2={height} stroke={color || defaultLineColor}
           strokeWidth="1" key={`vLine${index}`} />;
       })}
     </>
   );
 };
 
-// A dark border for the left and bottom of the graph
-interface IHalfBorder {
-  height: number;
-  width: number;
-}
-const HalfBorder = ({ height, width }: IHalfBorder) => (
+// A dark border for the left and bottom of the graph and a lighter border for the right
+const GraphBorder = () => (
   <>
-    <line x1="0" y1="0" x2="0" y2={height} stroke="black" strokeWidth="4" />
-    <line x1="0" y1={height} x2={width} y2={height} stroke="black" strokeWidth="4" />
+    <line x1={width + graphMargin} y1="0" x2={width + graphMargin} y2={height} stroke={defaultLineColor}
+      strokeWidth="1" />
+    <line x1={graphMargin} y1="0" x2={graphMargin} y2={height} stroke="black" strokeWidth="2" />
+    <line x1={graphMargin - 1} y1={height} x2={width + graphMargin + .5} y2={height} stroke="black" strokeWidth="2" />
   </>
 );
 
@@ -73,8 +75,6 @@ interface ISimGraph {
 export const SimGraph = ({
   ac, data, percentComplete, horizontalLabel, horizontalRange, verticalLabel, verticalRange, videoComplete
 }: ISimGraph) => {
-  const width = 307;
-  const height = 95;
   const plotColor = "#0481a0";
 
   // Determine how to convert between data space and svg space
@@ -82,7 +82,7 @@ export const SimGraph = ({
   const xSpan = xRange.max - xRange.min;
   const yRange = verticalRange || getRange(data, "y");
   const ySpan = yRange.max - yRange.min;
-  const convertX = (x: number) => (x - xRange.min) / xSpan * width;
+  const convertX = (x: number) => (x - xRange.min) / xSpan * width + graphMargin;
   const convertY = (y: number) => height - ((y - yRange.min) / ySpan * height);
 
   // Determine where the dot and end of plot should be
@@ -100,16 +100,16 @@ export const SimGraph = ({
       <div className="vertical-label">{verticalLabel || "Vertical Axis"}</div>
       <div className="sim-graph-right">
         <div className="sim-graph">
-          <svg viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg" fill="blue">
-            <Grid height={height} hLines={3} vLines={2} width={width} />
+          <svg viewBox={`0 0 ${width + 2 * graphMargin} ${height + graphMargin}`} xmlns="http://www.w3.org/2000/svg" fill="white">
+            <GraphBackgroundGrid hLines={3} vLines={2} />
             {videoComplete && <polyline points={polylinePoints(data, convertX, convertY)} fill="none"
               stroke={plotColor} strokeWidth={1.5} />}
             <polyline points={polylinePoints(polylineData, convertX, convertY)} fill="none" stroke={plotColor}
               strokeWidth={3} />
+            <GraphBorder />
             {percentComplete > 0 &&
               <circle cx={convertX(currentPoint.x)} cy={convertY(currentPoint.y)} r={6} fill={plotColor}
                 stroke="white" strokeWidth={2} />}
-            <HalfBorder height={height} width={width} />
           </svg>
         </div>
         <div className="horizontal-label">{horizontalLabel || "Horizontal Axis"}</div>
