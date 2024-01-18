c1f3c5d4435635589d0737970a75c972420eedf3
Teale Fristoe <fristoe@gmail.com>
2024-01-18 09:30:59 -0800

    Add play button.

--- src/components/simulation-settings.tsx
+++ src/components/simulation-settings.tsx
@@ -7,6 +7,11 @@ import HeartPerson from "../assets/new-sim/people/person-artery.svg";
 import NosePerson from "../assets/new-sim/people/person-nose.svg";
 // import PrefrontalCortexPerson from "../assets/new-sim/people/person-brain-prefrontal-cortex.svg";
 
+// import PauseButton from "../assets/new-sim/buttons/pause-button-on.svg";
+// import PauseButtonHover from "../assets/new-sim/buttons/pause-button-hover.svg";
+import PlayButton from "../assets/new-sim/buttons/play-button-on.svg";
+// import PlayButtonHover from "../assets/new-sim/buttons/play-button-hover.svg";
+
 import "./simulation-settings.scss";
 
 interface ISimulationSettingsProps {
@@ -23,6 +28,14 @@ export function SimulationSettings({ ac }: ISimulationSettingsProps) {
     <div className="simulation-settings">
       <div className="settings-header">
         Simulation Settings
+      </div>
+      <div className="setting video-control">
+        <div className="setting-title">Simulation Time</div>
+        <div className="video-control-row">
+          <button className="play-button">
+            <PlayButton />
+          </button>
+        </div>
       </div>
       <Person className="person" style={personStyle} />
     </div>
