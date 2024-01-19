dcaae97573fd966a18900f896900fd410610aefe
Teale Fristoe <fristoe@gmail.com>
2024-01-18 17:39:42 -0800

    Style play button and make it toggle.

--- src/components/simulation-settings.tsx
+++ src/components/simulation-settings.tsx
@@ -1,3 +1,4 @@
+import { clsx } from "clsx";
 import React from "react";
 
 import { AppContext } from "../hooks/use-app-context";
@@ -9,18 +10,15 @@ import HeartPerson from "../assets/new-sim/people/person-artery.svg";
 import NosePerson from "../assets/new-sim/people/person-nose.svg";
 import PrefrontalCortexPerson from "../assets/new-sim/people/person-brain-prefrontal-cortex.svg";
 
-// import PauseButton from "../assets/new-sim/buttons/pause-button-on.svg";
-// import PauseButtonHover from "../assets/new-sim/buttons/pause-button-hover.svg";
-import PlayButton from "../assets/new-sim/buttons/play-button-on.svg";
-// import PlayButtonHover from "../assets/new-sim/buttons/play-button-hover.svg";
-
 import "./simulation-settings.scss";
 
 interface ISimulationSettingsProps {
   ac: AppContext;
 }
 export function SimulationSettings({ ac }: ISimulationSettingsProps) {
-  const { control1, setControl1, control2, setControl2 } = useCommonState(ac);
+  const {
+    playingTissue, setPlayingTissue, playingCell, control1, setControl1, control2, setControl2
+  } = useCommonState(ac);
   const isBrain = ac.organ === "brain";
   const Person = ac.organ === "heart" ? HeartPerson
     : ac.organ === "nose" ? NosePerson
@@ -28,6 +26,7 @@ export function SimulationSettings({ ac }: ISimulationSettingsProps) {
   const personStyle = isBrain
     ? { bottom: "21px", right: "30px" }
     : { bottom: "84px", right: "32px" };
+  const playing = playingTissue || playingCell;
   return (
     <div className="simulation-settings">
       <div className="settings-header">
@@ -36,9 +35,10 @@ export function SimulationSettings({ ac }: ISimulationSettingsProps) {
       <div className="setting video-control">
         <div className="setting-title">Simulation Time</div>
         <div className="video-control-row">
-          <button className="play-button">
-            <PlayButton />
-          </button>
+          <button
+            className={clsx("play-button", { playing })}
+            onClick={() => setPlayingTissue(!playingTissue)}
+          />
         </div>
       </div>
       <ButtonToggle
