9ae054db12632f2720aa719eeb757e82ac787ccc
Teale Fristoe <fristoe@gmail.com>
2024-01-25 13:40:07 -0800

    186769543 Fix Checkboxes (#43)
    * Unify experiment state. Remove old commented code from simulation-app.tsx.

--- src/components/new-simulation/simulation-graphs-checkboxes.tsx
+++ src/components/new-simulation/simulation-graphs-checkboxes.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { clsx } from "clsx";
 import { useAppContext } from "../../hooks/use-app-context";
+import { ExperimentState } from "../../hooks/use-experiment-state";
+import { getAllExperiments } from "../../utils/control-utils";
 
 import KeyLine from "../../assets/new-sim/key-line.svg";
 
@@ -10,18 +12,17 @@ interface ExperimentDef {
 }
 
 interface ISimulationGraphsCheckboxesProps {
-  experiments: ExperimentDef[];
   control1: boolean;
   control2: boolean;
-  experimentsRun: boolean[][];
-  graphsShowing: boolean[][];
+  experimentState: ExperimentState[][];
   setGraphIsShowing: (c1: boolean, c2: boolean, value: boolean) => void;
 }
 
 export function SimulationGraphsCheckboxes(
-    { experiments, control1, control2, experimentsRun, graphsShowing, setGraphIsShowing }: 
+    { control1, control2, experimentState, setGraphIsShowing }: 
     ISimulationGraphsCheckboxesProps) {
   const ac = useAppContext();
+  const experiments = getAllExperiments(ac);
 
   function optionLabel(optionNumber: number, optionValue: boolean) {
     return ac.o(`SIMCONTROL${optionNumber}OPTION${optionValue ? 2 : 1}`);
@@ -33,23 +34,24 @@ export function SimulationGraphsCheckboxes(
   }
 
   const checkboxList = experiments.map((e, index) => {
-      const key = "" + (e.option1 ? "t" : "f") + (e.option2 ? "t" : "f");
-      const enabled = experimentsRun[+e.option1][+e.option2];
-      const selected = e.option1===control1 && e.option2===control2;
-      return (
-        <div className={clsx("checkbox-row", { enabled, selected })} key={key}>
-          <span>
-            <input id={"check" + key} type="checkbox" value={"val"+key} disabled={!enabled}
-              checked={graphsShowing[+e.option1][+e.option2]} 
-              onChange={(ev) => { setGraphIsShowing(e.option1, e.option2, ev.target.checked); }}
-              />
-            <label htmlFor={"check" + key}>
-              {optionLabelPair(e)}
-            </label>
-          </span>
-          {graphsShowing[+e.option1][+e.option2] && 
-            <KeyLine className={"line-style-" + index}/>}
-        </div>);    
+    const key = "" + (e.option1 ? "t" : "f") + (e.option2 ? "t" : "f");
+    const enabled = experimentState[+e.option1][+e.option2].seen;
+    const selected = e.option1===control1 && e.option2===control2;
+    const displayGraph = experimentState[+e.option1][+e.option2].displayGraph;
+    return (
+      <div className={clsx("checkbox-row", { enabled, selected })} key={key}>
+        <span>
+          <input id={"check" + key} type="checkbox" value={"val"+key} disabled={!enabled}
+            checked={displayGraph} 
+            onChange={(ev) => { setGraphIsShowing(e.option1, e.option2, ev.target.checked); }}
+            />
+          <label htmlFor={"check" + key}>
+            {optionLabelPair(e)}
+          </label>
+        </span>
+        {displayGraph && <KeyLine className={"line-style-" + index}/>}
+      </div>
+    );    
   });
 
   return (
@@ -57,5 +59,4 @@ export function SimulationGraphsCheckboxes(
       {checkboxList}
     </form>
   );
-
 }
