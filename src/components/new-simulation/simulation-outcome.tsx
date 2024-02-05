863004791396db0b0420227eaf1bb12b6a1a883c
Teale Fristoe <fristoe@gmail.com>
2024-02-05 06:27:53 -0800

    186963497 Update Outcome Styling (#48)
    * Update outcome styling.

--- src/components/new-simulation/simulation-outcome.tsx
+++ src/components/new-simulation/simulation-outcome.tsx
@@ -1,6 +1,8 @@
+import { clsx } from "clsx";
 import React from "react";
 
 import { useAppContext } from "../../hooks/use-app-context";
+import { getAllExperiments } from "../../utils/control-utils";
 
 import "./simulation-outcome.scss";
 
@@ -28,9 +30,32 @@ export function SimulationOutcome({ control1, control2, outcomeHidden, setOutcom
   if (outcomeHidden) {
     return <button className="simulation-button result" onClick={() => setOutcomeHidden(false)} />;
   } else {
+    // The border uses the same dashed style as the graph curves, so it must be an svg
+    const index = getAllExperiments(ac).findIndex(
+      controls => controls.option1 === control1 && controls.option2 === control2);
+    const borderClass = "line-style-" + index;
+    const strokeWidth = 3; // Keep in sync with .outcome-border stroke-width in simulation-outcome.scss
+    const halfStrokeWidth = strokeWidth / 2;
+    const borderHeight = 48; // Based on .outcome-area height in simulation-outcome.scss - margin space
+    const borderWidth = 392; // Based on .outcome-area width in simulation-outcome.scss - margin space
     return (
       <div className="outcome-area">
-        {ac.o(message)}
+        <svg
+          className="outcome-svg"
+          height={borderHeight} 
+          width={borderWidth}
+          xmlns="http://www.w3.org/2000/svg" 
+        >
+          <rect
+            x={halfStrokeWidth}
+            y={halfStrokeWidth}
+            height={borderHeight - strokeWidth}
+            width={borderWidth - strokeWidth}
+            rx={2}
+            className={clsx("outcome-border", borderClass)}
+          />
+        </svg>
+        {`${ac.t("OUTCOMEPREFIX")}${ac.o(message)}`}
         <button className="hide-button" onClick={() => setOutcomeHidden(true)} />
       </div>
     );
